795b0d113a8cc02d9d4edcf78eb67b62c81382d6
Студент 0 2 <student_02@sakhgu.net>
2025-06-07 10:28:51 +1100

    lab

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@
         <p>Группа: 231</p>
         <a href="http://localhost:3000/lab1/%D0%BA%D0%B0%D0%BB%D0%BA/static/html/index.html">ЛАБ 1</a>
         <a href="http://localhost:3000/lab2/todolist/html/index.html">ЛАБ 2</a>
+        <a href="http://localhost:3000/lab3/api.html">ЛАБ 3</a>
+
       `;
     });
  
